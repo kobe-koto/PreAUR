6f444321e01bb7156e595dcd50e3d6b0d7c9009a
kobe koto <91669733+kobe-koto@users.noreply.github.com>
2026-06-13 14:56:57 +0800

    ci: Generate Release Notes when release

--- .github/generate-release-notes.sh
+++ .github/generate-release-notes.sh
@@ -3,11 +3,13 @@
 VERSION_NO_V=$(echo $RELEASE_VERSION | sed 's/^v//')
 export VERSION_NO_V
 
-echo "## Release $RELEASE_VERSION" > release.md
-echo "" >> release.md
-echo "New version has been released!" >> release.md
-echo "" >> release.md
-echo "### Changes" >> release.md
-echo "" >> release.md
-git log --pretty=format:"* %s" $(git describe --tags --abbrev=0 HEAD^ 2>/dev/null || git rev-list --max-parents=0 HEAD)..HEAD >> release.md
-echo "" >> release.md
+(
+    echo "## Release $RELEASE_VERSION"
+    echo ""
+    echo "New version has been released!"
+    echo ""
+    echo "### Changes"
+    echo ""
+    git log --pretty=format:"* %s" $(git describe --tags --abbrev=0 HEAD^ 2>/dev/null || git rev-list --max-parents=0 HEAD)..HEAD
+    echo ""
+) > release.md
